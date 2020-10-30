9873785a9e79e9aca45726c3b3c6a3cad4ff4f44
Joelgamer2 <joelgamer2@outlook.com>
2020-10-30 19:33:56 +0100

    Pilares de marmol rojo,negro,verde añadidos y el blanco optimizado aun mas

--- src/main/java/HazardCraft/Iniciar/Bloques.java
+++ src/main/java/HazardCraft/Iniciar/Bloques.java
@@ -23,6 +23,15 @@ import HazardCraft.Bloques.mena_de_zafiro;
 import HazardCraft.Bloques.pilar_de_marmol_blanco;
 import HazardCraft.Bloques.pilar_de_marmol_blanco_inferior;
 import HazardCraft.Bloques.pilar_de_marmol_blanco_superior;
+import HazardCraft.Bloques.pilar_de_marmol_negro;
+import HazardCraft.Bloques.pilar_de_marmol_negro_inferior;
+import HazardCraft.Bloques.pilar_de_marmol_negro_superior;
+import HazardCraft.Bloques.pilar_de_marmol_rojo;
+import HazardCraft.Bloques.pilar_de_marmol_rojo_inferior;
+import HazardCraft.Bloques.pilar_de_marmol_rojo_superior;
+import HazardCraft.Bloques.pilar_de_marmol_verde;
+import HazardCraft.Bloques.pilar_de_marmol_verde_inferior;
+import HazardCraft.Bloques.pilar_de_marmol_verde_superior;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
@@ -48,6 +57,18 @@ public class Bloques
 	public static Block PILAR_DE_MARMOL_BLANCO = new pilar_de_marmol_blanco("pilar_de_marmol_blanco", Material.ROCK);
 	public static Block PILAR_DE_MARMOL_BLANCO_INFERIOR = new pilar_de_marmol_blanco_inferior("pilar_de_marmol_blanco_inferior", Material.ROCK);
 	public static Block PILAR_DE_MARMOL_BLANCO_SUPERIOR = new pilar_de_marmol_blanco_superior("pilar_de_marmol_blanco_superior", Material.ROCK);
+	
+	public static Block PILAR_DE_MARMOL_VERDE = new pilar_de_marmol_verde("pilar_de_marmol_verde", Material.ROCK);
+	public static Block PILAR_DE_MARMOL_VERDE_INFERIOR = new pilar_de_marmol_verde_inferior("pilar_de_marmol_verde_inferior", Material.ROCK);
+	public static Block PILAR_DE_MARMOL_VERDE_SUPERIOR = new pilar_de_marmol_verde_superior("pilar_de_marmol_verde_superior", Material.ROCK);
+	
+	public static Block PILAR_DE_MARMOL_NEGRO = new pilar_de_marmol_negro("pilar_de_marmol_negro", Material.ROCK);
+	public static Block PILAR_DE_MARMOL_NEGRO_INFERIOR = new pilar_de_marmol_negro_inferior("pilar_de_marmol_negro_inferior", Material.ROCK);
+	public static Block PILAR_DE_MARMOL_NEGRO_SUPERIOR = new pilar_de_marmol_negro_superior("pilar_de_marmol_negro_superior", Material.ROCK);
+	
+	public static Block PILAR_DE_MARMOL_ROJO = new pilar_de_marmol_rojo("pilar_de_marmol_rojo", Material.ROCK);
+	public static Block PILAR_DE_MARMOL_ROJO_INFERIOR = new pilar_de_marmol_rojo_inferior("pilar_de_marmol_rojo_inferior", Material.ROCK);
+	public static Block PILAR_DE_MARMOL_ROJO_SUPERIOR = new pilar_de_marmol_rojo_superior("pilar_de_marmol_rojo_superior", Material.ROCK);
 	
 	public static final Block MENA_DE_COBRE = new mena_de_cobre("mena_de_cobre", Material.ROCK);
 	public static final Block MENA_DE_ZAFIRO = new mena_de_zafiro("mena_de_zafiro", Material.ROCK);
