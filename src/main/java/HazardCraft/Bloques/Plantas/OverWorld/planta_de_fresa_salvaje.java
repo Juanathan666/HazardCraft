d789c0a0e2d72c28abf08180c3e4c58bacb5aebf
Joelgamer2 <joelgamer2@outlook.com>
2021-01-22 18:20:51 +0100

    planta salvajes corregidas

--- src/main/java/HazardCraft/Bloques/Plantas/OverWorld/planta_de_fresa_salvaje.java
+++ src/main/java/HazardCraft/Bloques/Plantas/OverWorld/planta_de_fresa_salvaje.java
@@ -2,18 +2,37 @@ package HazardCraft.Bloques.Plantas.OverWorld;
 
 import java.util.Random;
 
+import javax.annotation.Nullable;
+
 import HazardCraft.Bloques.Registrar.Registro_Bloques;
 import HazardCraft.Items.Registrar.Items_OverWorld;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class planta_de_fresa_salvaje extends Registro_Bloques
 {
+	private static final AxisAlignedBB[] FRESA_AXIS = new AxisAlignedBB[] {new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.25D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.375D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.625D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.75D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.875D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D)};
+	
 	public static int cantidad = 1;
 
 	public planta_de_fresa_salvaje(String name, Material material) 
@@ -31,6 +50,9 @@ public class planta_de_fresa_salvaje extends Registro_Bloques
 		return Items_OverWorld.FRESA;
 	}
 	
+	
+	
+	
 	@Override
 	public int quantityDroppedWithBonus(int fortune, Random rand) 
 	{
@@ -44,6 +66,12 @@ public class planta_de_fresa_salvaje extends Registro_Bloques
 		return cantidad;
 	}
 	
+	 @SideOnly(Side.CLIENT)
+	    public BlockRenderLayer getBlockLayer()
+	    {
+	        return BlockRenderLayer.CUTOUT;
+	    }
+	
 	@Override
 	public boolean isOpaqueCube(IBlockState state) {
 		return false;
@@ -52,5 +80,17 @@ public class planta_de_fresa_salvaje extends Registro_Bloques
 	@Override
 	public boolean isFullCube(IBlockState state) {
 		return false;
+	}
+	
+	 @Nullable
+	    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
+	    {
+	        return NULL_AABB;
+	    }
+	
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) 
+	{
+		return FRESA_AXIS[((Integer)3).intValue()];
 	}
 }
--- src/main/java/HazardCraft/Bloques/Plantas/OverWorld/planta_de_tomate_salvaje.java
+++ src/main/java/HazardCraft/Bloques/Plantas/OverWorld/planta_de_tomate_salvaje.java
@@ -1,16 +1,62 @@
 package HazardCraft.Bloques.Plantas.OverWorld;
 
+import javax.annotation.Nullable;
+
 import HazardCraft.Bloques.Registrar.Registro_Bloques;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class planta_de_tomate_salvaje extends Registro_Bloques
 {
 
+	private static final AxisAlignedBB[] FRESA_AXIS = new AxisAlignedBB[] {new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.25D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.375D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.625D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.75D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.875D, 1.0D), 
+			   new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D)};
+	
 	public planta_de_tomate_salvaje(String name, Material material) 
 	{
 		super(name, material);
 		setSoundType(SoundType.PLANT);
+	}
+	
+	 @SideOnly(Side.CLIENT)
+	    public BlockRenderLayer getBlockLayer()
+	    {
+	        return BlockRenderLayer.CUTOUT;
+	    }
+	
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+	
+	 @Nullable
+	    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
+	    {
+	        return NULL_AABB;
+	    }
+	
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) 
+	{
+		return FRESA_AXIS[((Integer)3).intValue()];
 	}
 }
